ebbca143889697bef3e67161eb7ea6fe1ae65a51
Denis Kobzar <layfilms.today@gmail.com>
2023-05-06 15:45:43 +0300

    Second changer

---
+++ WikiScrapper/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiScrapper.MVVM.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        private int _clicks = 0;
+
+        public int Clicks
+        {
+            get => _clicks;
+            set
+            {
+                _clicks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Task.Delay(1000).Wait();
+                    Clicks++;
+                }
+            });
+        }
+    }
+}
